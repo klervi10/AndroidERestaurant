1ca2b9bb60f248d801218cc3066f53fa27fc1153
klervi10 <klervi.muros-le-rouzic@isen.yncrea.fr>
2024-03-18 13:06:36 +0100

    modelage page

--- app/src/main/java/fr/isen/muros/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/muros/androiderestaurant/HomeActivity.kt
@@ -3,49 +3,79 @@ package fr.isen.muros.androiderestaurant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.res.painterResource
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.unit.dp
 import fr.isen.muros.androiderestaurant.ui.theme.AndroidERestaurantTheme
 
-class MainActivity : ComponentActivity() {
+class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidERestaurantTheme {
                 // A surface container using the 'background' color from the theme
-                NameAndLogo("Android")
+                Welcome()
             }
         }
     }
 }
 
 @Composable
-fun NameAndLogo(name: String, modifier: Modifier = Modifier) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Image(
-            painter = painterResource(id = R.drawable.logo),
-            contentDescription = "Logo Android",
+fun Welcome(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "     DroidRestaurant",
+            color = Color.White,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            modifier = Modifier
+                .background(Color(0xFFFFA500))
+                .fillMaxWidth()
+                .padding(vertical = 16.dp) // Ajoute un espace vertical entre le texte et le haut de l'écran
+
         )
-        Column {
-            Text(
-                text = "Hello $name!",
-                modifier = modifier
-            )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Bienvenue",
+                    color = Color(0xFFFFA500),
+                    textAlign = TextAlign.Center
+                )
+                Text(
+                    text = "chez",
+                    color = Color(0xFFFFA500),
+                    textAlign = TextAlign.Center
+                )
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun NameAndLogoPreview() {
+fun WelcomePreview() {
     AndroidERestaurantTheme {
-        NameAndLogo("Android")
+        Welcome()
     }
 }
